25e20a941472b85e15f1ab9b9358866b5c9787e0
Mohammed Abdulrazeg <github@mzawi.me>
2023-09-01 09:30:34 +0100

    Using Testcontainers instead of EphemeralMongo

--- Backend.Api.Test/BackendApiFactory.cs
+++ Backend.Api.Test/BackendApiFactory.cs
@@ -1,41 +1,43 @@
 ﻿using Backend.Api.Data;
-using EphemeralMongo;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Testcontainers.MongoDb;
 
 namespace Backend.Api.Test;
 
 public sealed class BackendApiFactory : WebApplicationFactory<IApiMarker>
 {
-    private readonly MongoRunnerOptions _options = new()
-    {
-        UseSingleNodeReplicaSet = true, // Default: false
-        StandardOuputLogger = line => Console.WriteLine(line), // Default: null
-        StandardErrorLogger = line => Console.WriteLine(line), // Default: null
-        AdditionalArguments = "--quiet", // Default: null
-        MongoPort = 27777, // Default: random available port
-        KillMongoProcessesWhenCurrentProcessExits = true // Default: false
-    };
+    // private readonly MongoRunnerOptions _options = new()
+    // {
+    //     UseSingleNodeReplicaSet = true, // Default: false
+    //     StandardOuputLogger = line => Console.WriteLine(line), // Default: null
+    //     StandardErrorLogger = line => Console.WriteLine(line), // Default: null
+    //     AdditionalArguments = "--quiet", // Default: null
+    //     MongoPort = 27777, // Default: random available port
+    //     KillMongoProcessesWhenCurrentProcessExits = true // Default: false
+    // };
+    //
+    // private IMongoRunner _runner = null!;
 
-    private IMongoRunner _runner = null!;
-
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    private readonly MongoDbContainer _mongoDbContainer = new MongoDbBuilder().WithPortBinding(27017, true).Build();
+    
+    protected override async void ConfigureWebHost(IWebHostBuilder builder)
     {
-        _runner = MongoRunner.Run(_options);
+        await _mongoDbContainer.StartAsync();
 
         builder.ConfigureServices(collection =>
         {
             collection.RemoveAll(typeof(IMongoDbClientFactory));
             collection.AddSingleton<IMongoDbClientFactory>(_ =>
-                new MongoDbClientFactory("mongodb://127.0.0.1:27777", "books"));
+                new MongoDbClientFactory($"mongodb://127.0.0.1:{_mongoDbContainer.GetMappedPublicPort(27017)}", "books"));
         });
     }
 
-    public override ValueTask DisposeAsync()
+    public override async ValueTask DisposeAsync()
     {
-        _runner.Dispose();
-        return base.DisposeAsync();
+        await _mongoDbContainer.DisposeAsync();
+        await base.DisposeAsync();
     }
 }
