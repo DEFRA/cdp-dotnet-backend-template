8ef2cf5d4abfde899c3d5f46a9dd0ef489253d4e
Mohammed Abdulrazeg <github@mzawi.me>
2023-11-02 13:40:15 +0000

    Removed integration tests and added useful unit test

--- Backend.Api.IntegrationTest/ValidationError.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Backend.Api.IntegrationTest;
-
-public class ValidationError
-{
-    public string PropertyName { get; set; } = default!;
-
-    public string ErrorMessage { get; set; } = default!;
-}
