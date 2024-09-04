40bc8315437f5ab220f893cebae4b18d0324c671
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-09-04 13:50:52 +0100

    fixes template script

--- .github/template/apply.sh
+++ .github/template/apply.sh
@@ -8,5 +8,5 @@ REPO_NAME_PASCAL_CASE=$(echo "$REPO_NAME" | sed -r 's/(^|-)([a-z])/\U\2/g')
 find . -name .git -prune -o -name .github -prune -o -type f -exec sed -i "s/cdp-dotnet-backend-template/${REPO_NAME}/g" {} \;
 find . -name .git -prune -o -name .github -prune -o -type f -exec sed -i "s/Backend\.Api/${REPO_NAME_PASCAL_CASE}/g" {} \;
 find . -name .git -prune -o -name .github -prune -o -type f -exec sed -i "s/CDP C# ASP\.NET Backend Template/${REPO_NAME}/g" {} \;
-find . -name .git -prune -o -name .github -prune -o -depth -name '*Backend\.Api*' -execdir bash -c 'mv -i "$1" "${1//Backend\.Api/${{ env.REPO_NAME_PASCAL_CASE }}}"' bash {} \;   
+find . -name .git -prune -o -name .github -prune -o -depth -name '*Backend\.Api*' -execdir bash -c 'mv -i "$1" "${1//Backend\.Api/${REPO_NAME_PASCAL_CASE}}"' bash {} \;   
 mv CdpDotnetBackendTemplate.sln "${REPO_NAME_PASCAL_CASE}".sln  
