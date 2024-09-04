9e5c0ce8bd17618fc46e0c1df91206d2cf59505f
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-09-04 15:09:50 +0100

    removes -i flag on mv

--- .github/template/apply.sh
+++ .github/template/apply.sh
@@ -5,8 +5,11 @@ repository=$1
 REPO_NAME=$(echo "${repository}" | awk -F '/' '{print $2}')
 REPO_NAME_PASCAL_CASE=$(echo "$REPO_NAME" | sed -r 's/(^|-)([a-z])/\U\2/g')
 
+echo "applying template... renaming to ${REPO_NAME} and ${REPO_NAME_PASCAL_CASE}"
 find . -name .git -prune -o -name .github -prune -o -type f -exec sed -i "s/cdp-dotnet-backend-template/${REPO_NAME}/g" {} \;
 find . -name .git -prune -o -name .github -prune -o -type f -exec sed -i "s/Backend\.Api/${REPO_NAME_PASCAL_CASE}/g" {} \;
 find . -name .git -prune -o -name .github -prune -o -type f -exec sed -i "s/CDP C# ASP\.NET Backend Template/${REPO_NAME}/g" {} \;
-find . -name .git -prune -o -name .github -prune -o -depth -name '*Backend\.Api*' -execdir bash -c 'mv -i "$1" "${1//Backend\.Api/${REPO_NAME_PASCAL_CASE}}"' bash {} \;   
+find . -name .git -prune -o -name .github -prune -o -depth -name '*Backend\.Api*' -execdir bash -c 'mv -f "$1" "${1//Backend\.Api/${REPO_NAME_PASCAL_CASE}}"' bash {} \;   
+
+find . -name .git -prune -o -name .github -prune -o -depth -name '*Backend\.Api*' -execdir bash -c 'echo "$1" "${1//Backend\.Api/${REPO_NAME_PASCAL_CASE}}"' bash {} \;   
 mv CdpDotnetBackendTemplate.sln "${REPO_NAME_PASCAL_CASE}".sln  
