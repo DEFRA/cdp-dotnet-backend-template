a6428aa3cd52ce2adb70f62a9a4e041cccb3afe2
Phil Segal <phil@phisec.co.uk>
2024-11-05 16:17:43 +0000

    440667: Add service_version to logging

--- Backend.Api/Program.cs
+++ Backend.Api/Program.cs
@@ -56,6 +56,7 @@ static Logger ConfigureLogging(WebApplicationBuilder _builder)
    var logger = new LoggerConfiguration()
        .ReadFrom.Configuration(_builder.Configuration)
        .Enrich.With<LogLevelMapper>()
+       .Enrich.WithProperty("service.version", Environment.GetEnvironmentVariable("SERVICE_VERSION"))
        .CreateLogger();
    _builder.Logging.AddSerilog(logger);
    logger.Information("Starting application");
