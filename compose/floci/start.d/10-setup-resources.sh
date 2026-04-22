5d78ea70381646ba0f2c4b683319447aad90daeb
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2026-04-22 14:12:26 +0100

    removes env from floci script

--- compose/floci/start.d/10-setup-resources.sh
+++ compose/floci/start.d/10-setup-resources.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-export AWS_ENDPOINT_URL=http://localhost:4566
-
 # S3 buckets
-#aws s3 mb s3://my-bucket
+# aws s3 mb s3://my-bucket
 
 # SQS queues
-#aws sqs create-queue --queue-name my-queue
+# aws sqs create-queue --queue-name my-queue
