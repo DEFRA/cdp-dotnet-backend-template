9aeefa6c243240ce1f908c6852d8327cbccef023
Christopher Turner <23338096+christopherjturner@users.noreply.github.com>
2026-04-22 15:07:50 +0100

    switch to floci (#36)
    * switch to floci
    
    * removes env from floci script

---
+++ compose/floci/start.d/10-setup-resources.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# S3 buckets
+# aws s3 mb s3://my-bucket
+
+# SQS queues
+# aws sqs create-queue --queue-name my-queue
---
+++ compose/mongo/10-init.js
@@ -0,0 +1,6 @@
+// MongoDB init script
+// This can be used for inserting local test data when mongo starts up
+
+// Example:
+// db = db.getSiblingDB('cdp-node-backend-template')
+// db.foo.insertOne({bar: "baz"})
